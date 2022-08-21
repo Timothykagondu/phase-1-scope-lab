d8e7f046c4ba4afc7164eac766822dddb640a6ab
Timothy <timothy.2k21@gmail.com>
2022-08-21 17:29:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName(){
+    window.customerName;
+    customerName = customerName.toUpperCase();
+};
+function setBestCustomer(){
+    window.bestCustomer;
+    bestCustomer = 'not bob';
+};
+function overwriteBestCustomer() {
+    window.bestCustomer;
+    bestCustomer = 'maybe bob';
+};
+
+const leastFavoriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob';
+}
